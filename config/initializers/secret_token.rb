16b98e64a7dc816aee752ff1b9e6543c04083b4f
Backnol Yogendran <backnol@gmail.com>
2013-08-17 01:27:09 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kaveri::Application.config.secret_key_base = '53a38dc9d9b7d096e03289f2a8a0cec4854100cbe8a2a4f88559975f82f97751f55d1b8843b37f27393b635b54bd322a33b1efbf276f7249b0fcfd155f69a35d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kaveri::Application.config.session_store :cookie_store, key: '_kaveri_session'
