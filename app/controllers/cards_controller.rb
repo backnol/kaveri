79dad1196707c6e8817cdc3c32707ff38cb8f7af
Backnol Yogendran <backnol@niteroll.com>
2013-09-12 23:17:07 -0400

    Remove unused views

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,17 +1,13 @@
 class CardsController < ApplicationController
 
-  # GET /cards
   # GET /cards.json
   def index
     @cards = Card.all
-    render json: @cards
   end
 
-  # GET /cards/1
   # GET /cards/1.json
   def show
     @card = Card.find(params[:id])
-    render json: @card
   end
 
   private
