5d1b37fdee97e1cc8223ae754834cdd9246ce6c0
Backnol Yogendran <backnol@niteroll.com>
2013-09-25 21:49:58 -0400

    Use constant for twitter api

--- config/initializers/social_api_keys.rb
+++ config/initializers/social_api_keys.rb
@@ -1,6 +1,6 @@
 social_api_keys = HashWithIndifferentAccess.new(YAML::load(File.open('keys/social_apis.yml')))
 twitter_key = social_api_keys[:twitter]
-client = Twitter::REST::Client.new do |config|
+TWITTER = Twitter::REST::Client.new do |config|
   config.consumer_key        = twitter_key[:consumer_key]
   config.consumer_secret     = twitter_key[:consumer_secret]
   config.access_token        = twitter_key[:access_token]
